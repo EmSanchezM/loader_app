94f18b2ee17ea0ab60d71b019f93195b33910d84
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2022-12-24 11:09:37 -0600

    feat: structure three routes

---
+++ src/layouts/index.tsx
@@ -0,0 +1,9 @@
+import { PublicLayout } from './PublicLayout'
+import { HomeLayout } from './HomeLayout'
+import { DashboardLayout } from './DashboardLayout'
+
+export {
+    PublicLayout,
+    HomeLayout,
+    DashboardLayout
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,38 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { ErrorBoundary } from '../components/ErrorBoundary';
 
-import { PublicLayout} from '../layouts/PublicLayout';
+import { 
+    PublicLayout, 
+    HomeLayout, 
+    DashboardLayout 
+} from '../layouts';
+
+import { Login } from '../pages/auth/Login';
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <PublicLayout />,
         errorElement: <ErrorBoundary />,
+        children: [
+            {
+                index: true, 
+                element: <Login />
+            },
+            {
+                path: 'dashboard',
+                element: <div>Private route</div>,
+                children: [
+                    { index: true, element: <DashboardLayout /> }
+                ]
+            },
+            {
+                path: 'home',
+                element: <div>Private route</div>,
+                children: [
+                    { index: true, element: <HomeLayout /> }
+                ]
+            }
+        ]
     },
 ]);
